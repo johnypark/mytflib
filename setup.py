426b365ce1cf3c5858bd5bfe8f38ca42585c902b
johnypark <parkjohnyc@gmail.com>
2022-04-28 12:58:45 -0400

    Update package info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mytflib", # Replace with your own username
-    version="0.0.1.6",
+    version="0.0.1.8",
     author="John Park",
     author_email="parkjohnyc@gmail.com",
     description="My TensorFlow Library",
