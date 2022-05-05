e8ffe83ac7d38202def912f9a316b447119c55a5
johnypark <parkjohnyc@gmail.com>
2022-05-05 17:38:36 -0400

    add one_cycle_tf as requirement

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setuptools.setup(
     url="https://github.com/johnypark/mytflib",
     packages=setuptools.find_packages(),
     install_requires = ['tensorflow',
-                       #'tensorflow_addons',
                        'opencv-python',
-                       'tensorflow_addons @ git+https://github.com/johnypark/addons#egg=tensorflow_addons'],
+                       'tensorflow_addons @ git+https://github.com/johnypark/addons#egg=tensorflow_addons',
+                        'one_cycle_tf @ git+https://github.com/johnypark/one_cycle_scheduler_tf#egg=one_cycle_tf']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
