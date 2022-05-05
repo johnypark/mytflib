4dcce0059b5740fd5fe296f8c50c658d6a4a129d
johnypark <parkjohnyc@gmail.com>
2022-05-05 17:44:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires = ['tensorflow',
                        'opencv-python',
-                       'tensorflow_addons @ git+https://github.com/johnypark/addons#egg=tensorflow_addons',
-                        'one_cycle_tf @ git+https://github.com/johnypark/one_cycle_scheduler_tf#egg=one_cycle_tf']
+                       'tensorflow_addons @ git+https://github.com/johnypark/addons',
+                        'one_cycle_scheduler_tf @ git+https://github.com/johnypark/one_cycle_scheduler_tf']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
