23bdc1db39a944a7aa48b3a766bdaebc519c5223
johnypark <parkjohnyc@gmail.com>
2022-05-10 10:51:26 -0400

    Update multiple
    1. Remove tfrec size param
    2. Generalize image key
    3. Change python version >=3.7
    4. Change function name

--- mytflib/DataLoader.py
+++ mytflib/DataLoader.py
@@ -68,15 +68,14 @@ def parse_tfrecord_fn(example, TFREC_FORMAT):
     return example
 
 
-def decode_image(image_data_bytes, tfrec_image_sizes):
+def decode_image(image_data_bytes):
 
-    img = tf.image.decode_jpeg(image_data_bytes, channels=3)  # image format uint8 [0,255]
+    img = tf.io.decode_jpeg(image_data_bytes, channels=3)  # image format uint8 [0,255]
     img = tf.cast(img, tf.float32) / 255.0
-    img = tf.reshape(img, [*tfrec_image_sizes, 3])
     return img
 
 
-def read_tfrecord(example, TFREC_FORMAT, TFREC_SIZES , LABEL_NAME="spname", IMAGE_NAME="image"):
+def read_tfrecord(example, TFREC_FORMAT, LABEL_NAME, IMAGE_NAME="image"):
 
     LABELED_TFREC_FORMAT = TFREC_FORMAT
     #{ EXAMPLE OF TFREC FORMAT 
@@ -87,13 +86,13 @@ def read_tfrecord(example, TFREC_FORMAT, TFREC_SIZES , LABEL_NAME="spname", IMAG
     #    "family": tf.io.FixedLenFeature([], tf.int64)
     #}
     example = tf.io.parse_single_example(example, LABELED_TFREC_FORMAT)
-    image = decode_image(example[IMAGE_NAME], tfrec_image_sizes = TFREC_SIZES )
+    image = decode_image(example[IMAGE_NAME])
     label = example[LABEL_NAME]
     #hierarchy = how can you get the hierarchy? 
     return image, label
 
 
-def load_tfrec_dataset(filenames, tfrec_format, tfrec_sizes, label_name, labeled=True, ordered=False):
+def load_tfrec_dataset(filenames, tfrec_format, label_name, image_key, labeled=True, ordered=False):
     # Read from TFRecords. For optimal performance, reading from multiple files at once and
     # disregarding data order. Order does not matter since we will be shuffling the data anyway.
     ignore_order = tf.data.Options()
@@ -102,9 +101,9 @@ def load_tfrec_dataset(filenames, tfrec_format, tfrec_sizes, label_name, labeled
     dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO) # automatically interleaves reads from multiple files
     dataset = dataset.with_options(ignore_order) # uses data as soon as it streams in, rather than in its original order
     dataset = dataset.map(lambda Example: read_tfrecord(Example, 
-                                                        TFREC_FORMAT = tfrec_format, 
-                                                        TFREC_SIZES = tfrec_sizes,
-                                                        LABEL_NAME = label_name))# if labeled else read_unlabeled_tfrecord, num_parallel_calls=AUTO)
+                                                        TFREC_FORMAT = tfrec_format,
+                                                        LABEL_NAME = label_name,
+                                                        IMAGE_NAME = image_key))# if labeled else read_unlabeled_tfrecord, num_parallel_calls=AUTO)
     return dataset
 
 
@@ -151,11 +150,11 @@ def get_train_ds_tfrec(LS_FILENAMES, TFREC_DICT, TFREC_SIZES, RESIZE_FACTOR, NUM
     dataset = dataset.prefetch(AUTO) # prefetch next batch while training (autotune prefetch buffer size)
     return dataset
 
-def get_train_ds_tfrec_from_dict(config_dict, label_name, DataRepeat = False, MoreAugment = False, Nsuffle = 2048):
+def get_train_ds_tfrec_from_dict(config_dict, label_name, image_key = "image", DataRepeat = False, MoreAugment = False, Nsuffle = 2048):
     
     LS_FILENAMES =  config_dict["ls_train_files"]
     TFREC_DICT =  config_dict["tfrec_structure"]
-    TFREC_SIZES =  config_dict["tfrec_shape"]
+    #TFREC_SIZES =  config_dict["tfrec_shape"]
     RESIZE_FACTOR =  config_dict["resize_resol"]
     NUM_CLASSES =  config_dict["N_cls"]
     BATCH_SIZE =  config_dict["batch_size"]
@@ -163,8 +162,9 @@ def get_train_ds_tfrec_from_dict(config_dict, label_name, DataRepeat = False, Mo
     tfrec_format = tfrec_format_generator(TFREC_DICT)
     dataset = load_tfrec_dataset(LS_FILENAMES, 
                                  tfrec_format = tfrec_format, 
-                                 tfrec_sizes = TFREC_SIZES,
-                                 label_name = label_name)
+                                 #tfrec_sizes = TFREC_SIZES,
+                                 label_name = label_name,
+                                 image_key = image_key)
     
     dataset = dataset.map(lambda image, label: onehot(image, label, n_cls = NUM_CLASSES), num_parallel_calls=AUTO)
     dataset = dataset.map(normalize_RGB, num_parallel_calls=AUTO).prefetch(AUTO)
@@ -189,11 +189,11 @@ def get_vali_ds_tfrec(LS_FILENAMES, TFREC_DICT, TFREC_SIZES, RESIZE_FACTOR, NUM_
     return dataset
 
 
-def get_vali_ds_tfrec_from_dict(config_dict, label_name, MoreAugment = False):
+def get_vali_ds_tfrec_from_dict(config_dict, label_name,  image_key = "image", MoreAugment = False):
     
     LS_FILENAMES =  config_dict["ls_vali_files"]
     TFREC_DICT =  config_dict["tfrec_structure"]
-    TFREC_SIZES =  config_dict["tfrec_shape"]
+    #TFREC_SIZES =  config_dict["tfrec_shape"]
     RESIZE_FACTOR =  config_dict["resize_resol"]
     NUM_CLASSES =  config_dict["N_cls"]
     BATCH_SIZE =  config_dict["batch_size"]
@@ -201,8 +201,10 @@ def get_vali_ds_tfrec_from_dict(config_dict, label_name, MoreAugment = False):
     tfrec_format = tfrec_format_generator(TFREC_DICT)
     dataset = load_tfrec_dataset(LS_FILENAMES, 
                                  tfrec_format = tfrec_format, 
-                                 tfrec_sizes = TFREC_SIZES,
-                                 label_name = label_name)
+                                 #tfrec_sizes = TFREC_SIZES,
+                                 label_name = label_name,
+                                 image_key = image_key)
+    
     dataset = dataset.map(lambda image, label: onehot(image, label, n_cls = NUM_CLASSES), num_parallel_calls=AUTO)
     dataset = dataset.map(normalize_RGB, num_parallel_calls=AUTO).prefetch(AUTO)
     dataset = dataset.map(lambda image, label: augment_images(image, label, resize_factor = RESIZE_FACTOR), num_parallel_calls=AUTO)
@@ -212,8 +214,8 @@ def get_vali_ds_tfrec_from_dict(config_dict, label_name, MoreAugment = False):
     return dataset
 
 
-def count_tfrec_items(filenames):
-    # the number of data items is written in the name of the .tfrec files, i.e. flowers00-230.tfrec = 230 data items
+def count_dps_in_tfrec(filenames):
+    # count data points in tfrec
     n = [int(re.compile(r"-([0-9]*)\.").search(filename).group(1)) for filename in filenames]
     return np.sum(n)
 
@@ -221,8 +223,8 @@ def count_tfrec_items(filenames):
 def get_filenames(DS_PATH):
     TRAINING_FILENAMES = tf.io.gfile.glob(DS_PATH + '/*train*.tfrec')#+tf.io.gfile.glob(GCS_DS_PATH_2 + '/*.tfrec')+tf.io.gfile.glob(GCS_DS_PATH_3 + '/*.tfrec')
     VALIDATION_FILENAMES = tf.io.gfile.glob(DS_PATH + '/*val*.tfrec')
-    NUM_TRAINING_IMAGES = count_tfrec_items(TRAINING_FILENAMES)
-    NUM_VALIDATION_IMAGES = count_tfrec_items(VALIDATION_FILENAMES)
+    NUM_TRAINING_IMAGES = count_dps_in_tfrec(TRAINING_FILENAMES)
+    NUM_VALIDATION_IMAGES = count_dps_in_tfrec(VALIDATION_FILENAMES)
     print('Dataset: {} training images, {} validation images'.format(NUM_TRAINING_IMAGES, NUM_VALIDATION_IMAGES))
     return TRAINING_FILENAMES, VALIDATION_FILENAMES, NUM_TRAINING_IMAGES
     
